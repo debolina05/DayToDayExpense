23ae62e16820e079de0a486879f58db372df13ae
unknown <moulisaha123@gmail.com>
2023-08-26 13:55:31 +0530

    sign-up

--- controllers/signup.js
+++ controllers/signup.js
@@ -15,15 +15,23 @@ module.exports.getSignupController = async (req, res) => {
 
 module.exports.postSignupController = async (req, res) => {
     console.log(req.body);
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
     const signup={
-        id:req.body.id,
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password,
-      }
-      Signup.create(signup)
+        name:name,
+        email:email,
+        password:password,
+    }
       try{
-        res.sendFile(path.join(__dirname, '/..', '/views', '/signup.html'))
+        const check = await Signup.findOne({where: {email:email}});
+        if(check){
+            res.send('<h1 style="color:red;">User Already Exists!!!</h1>');
+        }else{
+            Signup.create(signup);
+            res.sendFile(path.join(__dirname, '/..', '/views', '/signup.html'))
+        }
+        
       }catch(err){
         console.log(err);
         res.status(500).send('Internal server error');
